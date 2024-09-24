0bbb034dadd6ac172a868f0881076e82504d48a6
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-24 11:10:39 -0400

    Server, GET enpoint and model func. done

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+ try {
+    const cars = await Cars.getAll();
+    res.json(cars);
+  } catch(err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+    res.json(`getting car with id ${req.params.id}`)
+});
+
+router.post('/', async (req, res, next) => {
+   res.json('creating new car')
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message || 'Problem with the server'
+    });
+});
+
+module.exports = router;
