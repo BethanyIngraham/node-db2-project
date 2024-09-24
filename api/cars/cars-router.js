c9ee99ff37cb129f7669fcf7e060d60fe0053c54
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-24 16:30:09 -0400

    GET by id endpoint, middleware, model func. working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const Cars = require('./cars-model');
+const {
+    checkCarId,
+    checkCarPayload, 
+    checkVinNumberUnique, 
+    checkVinNumberValid
+} = require('./cars-middleware');
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
- try {
-    const cars = await Cars.getAll();
-    res.json(cars);
-  } catch(err) {
-    next(err);
-  }
+    try {
+        const cars = await Cars.getAll();
+        res.json(cars);
+    } catch(err) {
+        next(err);
+    }
 });
 
-router.get('/:id', async (req, res, next) => {
-    res.json(`getting car with id ${req.params.id}`)
+router.get('/:id', checkCarId, async (req, res, next) => {
+    try {
+        res.json(req.car);
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.post('/', async (req, res, next) => {
