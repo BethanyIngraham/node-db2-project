56e8c99c64fe6e06123f748f2d01078fd37c7171
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-24 00:55:30 -0400

    Db schema, seed and scripts created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
 // STRETCH
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert([
+        {vin: '1234ABCD98NW457VA', make: 'Toyota', model: 'Yaris', mileage: 90300},
+        {vin: '098BGOA461SAXL331', make: 'Honda', model: 'Odyssey', mileage: 100750},
+        {vin: 'OPV467AQ2V9L260NE', make: 'Kia', model: 'Sorento', mileage: 70560},
+        {vin: 'ZXT258F1SA7NW357P', make: 'Hyundai', model: 'Tuscon', mileage: 45000}
+    ]);
+}
